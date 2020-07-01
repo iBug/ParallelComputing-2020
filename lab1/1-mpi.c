224f990653ce65bdfcf25deed3d1dd0d4ea89a36
iBug <git@ibugone.com>
2020-07-02 02:09:49 +0800

    Lab 1: 1-mpi done

---
+++ lab1/1-mpi.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mpi.h>
+
+typedef unsigned char byte;
+
+int main(int argc, char **argv) {
+    unsigned n;
+    int rank, size; // MPI
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        scanf(" %u", &n);
+        n += 1;
+    }
+    if (n < size * size) {
+        fprintf(stderr, "Unexpected number %u, need at least %d for %d processes.\n", n, size * size, size);
+    }
+    MPI_Bcast(&n, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+
+    unsigned start, end, length;
+    start = n * rank / size;
+    end = n * (rank + 1) / size;
+    length = end - start;
+
+    byte *prime = malloc(length);
+    memset(prime, 1, length);
+    if (rank == 0) {
+        prime[0] = prime[1] = 0;
+    }
+    unsigned i, k = 2;
+    while (k * k <= n) {
+        if (k * k >= start) {
+            i = k * k - start;
+        } else {
+            i = ((start - 1) / k + 1) * k - start;
+        }
+        for (; i < length; i += k)
+            prime[i] = 0;
+        if (rank == 0) {
+            // Find next prime
+            for (i = k + 1; prime[i] == 0; i++);
+            k = i;
+        }
+        MPI_Bcast(&k, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+    }
+
+    unsigned cnt = 0, total = 0;
+    for (unsigned i = 0; i < length; i++) {
+        if (prime[i])
+            cnt++;
+    }
+    free(prime);
+
+    MPI_Reduce(&cnt, &total, 1, MPI_UNSIGNED, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        printf("%u\n", total);
+    }
+    MPI_Finalize();
+    return 0;
+}
