57114a264aef69ac206815bcbc9868223c5e7e36
iBug <git@ibugone.com>
2020-07-11 00:12:01 +0800

    Lab 4: Multi-merge implemented

--- lab4/main.c
+++ lab4/main.c
@@ -4,9 +4,8 @@
 #include <math.h>
 #include <mpi.h>
 
-#define G 6.67e-11
-#define MASS 1e4
-#define FRAME 1e-3
+#define HLEFT(index) (2 * ((index) + 1))
+#define HRIGHT(index) (2 * ((index) + 2))
 
 void qsort_recurse(int *a, int n) {
     if (n <= 5)
@@ -39,6 +38,84 @@ void quick_sort(int *a, int n) {
     }
 }
 
+void multi_merge(int *data, int k, const int *sizes) {
+    int total = 0;
+    for (int i = 0; i < k; i++) {
+        total += sizes[i];
+    }
+    int *list = malloc(total * sizeof(int));    // Scratch area
+    int listsize = 0;
+    int *index = malloc((k + 1) * sizeof(int)); // Start index of each "way"
+    int *ptr = malloc(k * sizeof(int));         // Current index in each "way"
+    int *heap = malloc(k * sizeof(int));
+    int heapsize = k; // If a way has been merged completely we need to decrement heap size
+    index[0] = 0;
+    for (int i = 0; i < k; i++) {
+        ptr[i] = index[i];
+        index[i + 1] = index[i] + sizes[i];
+        heap[i] = i;
+    }
+
+    // Multi-way merge using heap
+    // heap[i] means the "head" element of way i
+    // So the actual element is data[ptr[heap[i]]]
+    for (int i = (heapsize - 1) / 2; i > 0; i--) {
+        int smallest = i;
+        if (HLEFT(i) < heapsize && data[ptr[heap[smallest]]] > data[ptr[heap[HLEFT(i)]]]) {
+            smallest = HLEFT(i);
+        }
+        if (HRIGHT(i) < heapsize && data[ptr[heap[smallest]]] > data[ptr[heap[HRIGHT(i)]]]) {
+            smallest = HRIGHT(i);
+        }
+        if (smallest != i) {
+            int t = heap[i];
+            heap[i] = heap[smallest];
+            heap[smallest] = t;
+        }
+    }
+    while (1) {
+        list[listsize++] = data[ptr[heap[0]]];;
+        ptr[heap[0]]++;
+        if (ptr[heap[0]] >= index[heap[0] + 1]) {
+            // This "way" reached its end
+            heap[0] = heap[--heapsize];
+            if (heapsize == 0)
+                break;
+        }
+
+        for (int i = 0, smallest;; i = smallest) {
+            smallest = i;
+            if (HLEFT(i) < heapsize && data[ptr[heap[smallest]]] > data[ptr[heap[HLEFT(i)]]]) {
+                smallest = HLEFT(i);
+            }
+            if (HRIGHT(i) < heapsize && data[ptr[heap[smallest]]] > data[ptr[heap[HRIGHT(i)]]]) {
+                smallest = HRIGHT(i);
+            }
+            if (smallest == i)
+                break;
+            int t = heap[i];
+            heap[i] = heap[smallest];
+            heap[smallest] = t;
+        }
+    }
+    free(index);
+    free(ptr);
+    free(heap);
+
+    for (int i = 0; i < listsize; i++) {
+        data[i] = list[i];
+    }
+    free(list);
+}
+
+void multi_merge_flat(int *data, int k, int eachsize) {
+    int *sizes = malloc(k * sizeof(int));
+    for (int i = 0; i < k; i++)
+        sizes[i] = eachsize;
+    multi_merge(data, k, sizes);
+    free(sizes);
+}
+
 int main(int argc, char **argv) {
     int rank, size; // MPI
     int n;
@@ -69,7 +146,6 @@ int main(int argc, char **argv) {
     const int this_start = displs[rank];
     const int this_size = block_sizes[rank];
     int *this_data = malloc(this_size * sizeof(int));
-    int *pivots = malloc((size - 1) * sizeof(int));
 
     // Dispatch first batch of jobs
     MPI_Scatterv(root_data, block_sizes, displs, MPI_INT, this_data, this_size, MPI_INT, 0, MPI_COMM_WORLD);
@@ -78,7 +154,23 @@ int main(int argc, char **argv) {
     quick_sort(this_data, this_size);
 
     // Sampling
-    // Unfinished
+    int *samples = malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        samples[i] = this_data[this_size * (i + 1) / (size + 1)];
+    }
+    MPI_Gather(samples, size, MPI_INT, root_samples, size * size, MPI_INT, 0, MPI_COMM_WORLD);
+    free(samples);
+
+    int *pivots = malloc((size - 1) * sizeof(int));
+    if (rank == 0) {
+        multi_merge_flat(root_samples, size, size);
+        for (int i = 0; i < size - 1; i++) {
+            pivots[i] = root_samples[size * (i + 1)];
+        }
+    }
+    MPI_Bcast(pivots, size - 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Partition by pivots
 
     MPI_Finalize();
     free(block_sizes);
