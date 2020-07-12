a79a1b55a3acb7243b4eaa9f39b3c12e71d638ec
iBug <git@ibugone.com>
2020-07-12 21:15:43 +0800

    Common: Refine time calculator script

--- reporttime.pl
+++ reporttime.pl
@@ -4,9 +4,8 @@ use v5.10;
 use List::Util qw/sum/;
 use strict;
 
-chomp(my @data = <>);
-@data = sort {$a <=> $b} @data;
-
-my $cut = int 0.5 + (scalar @data) * 0.20;
+my @data = sort {$a <=> $b} <>;
+die "Empty content?\n" unless @data;
+my $cut = int 0.5 + scalar @data * 0.20;
 @data = @data[$cut..$#data - $cut];
-say sum(@data)/ scalar(@data);
+say sum(@data)/ scalar @data;
